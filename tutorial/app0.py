b5e695b35663c4abab85e729706980fd2840a5ec
JRM <jordeam@gmail.com>
2021-06-02 17:50:12 -0300

    Changing base directory

---
+++ tutorial/app0.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 31 13:59:55 2021
+
+@author: jrm
+"""
+
+from flask import Flask
+
+app = Flask('__name__')
+
+@app.route('/')
+def index():
+    return '<h1> Hello there!!!!</h1>'
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000, debug=True)
+
