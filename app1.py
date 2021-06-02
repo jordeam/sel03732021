721bba81b9640a67040a8a54d030503d72253e2e
JRM <jordeam@gmail.com>
2021-06-02 14:39:49 -0300

    Initial commit

---
+++ app1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 31 13:59:55 2021
+
+@author: jrm
+"""
+
+from flask import Flask, render_template
+
+devices_all = [
+    {
+     'title': 'Device 1',
+     'owner': 'Me',
+     'content': 'Contents of device 1'
+    },
+    {
+     'title': 'Devices 2',
+     'content': 'Contents of device 2'
+    },
+    ]
+
+app = Flask('__name__')
+
+@app.route('/')
+def index():
+    return '''
+<h1> Hello there! </h1>
+'''
+# Uses a simple template, with no variable content
+@app.route('/device')
+def device():
+    return render_template('device.html')
+
+@app.route('/main')
+def main():
+    return render_template('index.html')
+
+@app.route('/devices')
+def posts():
+    return render_template('devices.html', devices=devices_all)
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000, debug=True)
