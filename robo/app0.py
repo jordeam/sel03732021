01214b11764e71f8e1720f0871d0d78afe2e0c61
Victor M. Arzolla <victor.arzolla@gmail.com>
2021-06-30 17:07:29 -0300

    Atualizando controller.html

--- robo/app0.py
+++ robo/app0.py
@@ -9,11 +9,19 @@ Created on Mon May 31 13:59:55 2021
 from flask import Flask, render_template
 
 
-app = Flask('__name__')
+app = Flask(__name__, template_folder='templates')
+
 
 @app.route('/')
 def index():
     return render_template('controller.html')
+
+
+@app.route('/joystick/<string:s>')
+def joystick(s):
+    x, y = s.split(',')
+    return render_template('index.html', s=s)
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000, debug=True)
