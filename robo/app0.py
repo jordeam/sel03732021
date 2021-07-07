575c57c5904c48b24aa9cc753aa868edd07baac5
Victor M. Arzolla <victor.arzolla@gmail.com>
2021-07-07 16:01:27 -0300

    Adicionada comunicação por botão

--- robo/app0.py
+++ robo/app0.py
@@ -6,10 +6,12 @@ Created on Mon May 31 13:59:55 2021
 @author: jrm
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, jsonify
 
+from flask_socketio import SocketIO, emit, send
 
 app = Flask(__name__, template_folder='templates')
+io = SocketIO(app)
 
 
 @app.route('/')
@@ -17,10 +19,11 @@ def index():
     return render_template('controller.html')
 
 
-@app.route('/joystick/<string:s>')
-def joystick(s):
-    x, y = s.split(',')
-    return render_template('index.html', s=s)
+@io.on('get_input')
+def get_input_handler(msg):
+    print(msg)
+
+
 
 
 if __name__ == '__main__':
