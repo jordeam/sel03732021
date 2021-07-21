9054678abcd5e89fd888e48f9a8ee4f862abcab8
GabrielFaglioni <56044094+GabrielFaglioni@users.noreply.github.com>
2021-07-21 15:24:00 -0300

    Update models.py

--- smart/models.py
+++ smart/models.py
@@ -2,7 +2,7 @@ from flask_login import UserMixin
 from . import db
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True) # primary keys são requeridas pela SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     #confirma = db.Column(db.String(100))
