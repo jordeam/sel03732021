aad0ac535b261cda122144a3fef1611f05f867b3
Victor M. Arzolla <victor.arzolla@gmail.com>
2021-07-21 16:08:53 -0300

    Implementados dois listeners

--- robo/app0.py
+++ robo/app0.py
@@ -22,20 +22,26 @@ def index():
     return render_template('controller.html')
 
 
-
-msg_antes = 0
+pos_1_antes = [0,0]
+pos_2_antes = [0,0]
 
 @sio.on('get_input')
-def get_input_handler(msg):
+def get_input_handler(data):
 
-    global msg_antes
+    global pos_1_antes, pos_2_antes
+    pos_1 = [int(data['X1']),int(data['Y1'])]
+    pos_2 = [int(data['X2']),int(data['Y2'])]
     # roda apenas se a mensagem foi alterada 
-    if msg_antes != msg:
-        print('Enviando coordenadas:\n',msg)
-        msg_antes = msg
+    if pos_1_antes != pos_1:
+        print('Enviando coordenadas robo1:\n',pos_1)
+        pos_1_antes = pos_1
         # emite o input para clients conectados
-        sio.emit('robot_get_input', msg)
-        
+        sio.emit('robot1_get_input', pos_1)
+    if pos_2_antes != pos_2:
+        print('Enviando coordenadas robo2:\n',pos_2)
+        pos_2_antes = pos_2
+        # emite o input para clients conectados
+        sio.emit('robot2_get_input', pos_2)      
 
 
 
--- robo/listen1.py
+++ robo/listen1.py
@@ -5,10 +5,6 @@ sio = socketio.Client()
 
 server_ip = 'http://192.168.0.4:5000'
 
-pos_X = 0
-pos_Y = 0
-gdata = 0
-
 
 @sio.event
 def connect():
@@ -22,20 +18,25 @@ def connect_error(data):
 def disconnect():
     print("Desconectado do server!")
 
-@sio.on('robot_get_input')
+pos_X = 0
+pos_Y = 0
+gdata = 0
+
+@sio.on('robot1_get_input')
 def on_message(data):
 
     global pos_X, pos_Y, gdata
-    pos_X = int(data['X'])
-    pos_Y = int(data['Y'])
+    pos_X = data[0]
+    pos_Y = data[1]
     gdata = data
     
 
 if __name__ == '__main__':
+    print('########### Controle 1 ###########')
     sio.connect(server_ip)
     msg_antes = 0
     while(1):
 
         if msg_antes != gdata:
-            print('X = ',pos_X,' ; ', 'Y = ',pos_Y)
+            print('Controle 1:\n','X = ',pos_X,' ; ', 'Y = ',pos_Y)
             msg_antes = gdata
