36924fd70a5b7cdbb3c524049ee697b1b80e2ead
leosmarcondes <74574372+leosmarcondes@users.noreply.github.com>
2021-07-21 15:26:41 -0300

    Update main.py

--- smart/main.py
+++ smart/main.py
@@ -7,34 +7,36 @@ from gpiozero import LED
 led = LED(26)
 main = Blueprint('main', __name__)
 
-
 def gen(camera):
-    """Video streaming generator function."""
+    # Função de gerador de streaming de vídeo
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-
+# Cria a rota para página de home
 @main.route('/home')
 def index():
     return render_template('index.html')
 
+# Cria a rota para página de perfil
 @main.route('/profile')
 def profile():
     return render_template('profile.html', name=current_user.name)
 
 @main.route('/video_feed')
 def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
+    # Rota de streaming de vídeo.
     return Response(gen(camera_pi.Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+# Cria a rota para o botão de abrir a fechadura
 @main.route('/abrir', methods=['POST'])
 def abrir():
     led.on()
     return render_template('profile.html', name=current_user.name)
 
+# Cria a rota para o botão de fechar a fechadura
 @main.route('/fechar', methods=['POST'])
 def fechar():
     led.off()
